cea44bdeaab55707d598c6e9401804c8c6d9c537
Sunwook Hwang <koonuss@gmail.com>
2026-09-26 22:05:00 +0900

    Add seven colorscheme packages and annotate package groups

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -2,14 +2,26 @@
 -- ============ NATIVE PACKAGES ============
 -- =========================================
 local packages = {
+	-- themes
+	{ src = "https://github.com/folke/tokyonight.nvim" },
+	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+	{ src = "https://github.com/rebelot/kanagawa.nvim" },
+	{ src = "https://github.com/sainnhe/everforest" },
+	{ src = "https://github.com/EdenEast/nightfox.nvim" },
+	{ src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
+	{ src = "https://github.com/projekt0n/github-nvim-theme" },
+
+	-- UIs
 	{ src = "https://github.com/folke/snacks.nvim" },
 	{ src = "https://github.com/folke/persistence.nvim" },
 	{ src = "https://github.com/folke/which-key.nvim" },
 
+	-- git && navigation
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 	{ src = "https://github.com/stevearc/aerial.nvim" },
 	{ src = "https://github.com/Bekaboo/dropbar.nvim" },
 
+	-- lsp && format
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
